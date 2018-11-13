3607d85f2e8c03ee010de6c611de5a12e1300f0e
69lem <69lem@10.50.137.183>
2018-11-13 11:43:26 +0100

    Lavet et par metoder i Member.java og lavet en testClass og testPackage

--- Delfinen/src/Data/Member.java
+++ Delfinen/src/Data/Member.java
@@ -6,10 +6,11 @@
 package Data;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 /**
  *
- * @author Kasper Jeppesen
+ * @author Kasper Jeppesen & Martin L.B.
  */
 public class Member
 {
@@ -20,10 +21,41 @@ public class Member
 
     private String name, address, city, emailAddress, memberType;
     private LocalDate dateOfBirth, dateOfJoining;
+
+    public Member
+            (boolean paidCurrentYear, boolean activityStatus, int zipCode,
+            int memberID, long phoneNumber, long arrearsBalance,
+            String name, String address, String city,
+            String emailAddress, String memberType, LocalDate dateOfBirth,
+            LocalDate dateOfJoining)
+    {
+        this.paidCurrentYear = paidCurrentYear;
+        this.activityStatus = activityStatus;
+        this.zipCode = zipCode;
+        this.memberID = memberID;
+        this.phoneNumber = phoneNumber;
+        this.arrearsBalance = arrearsBalance;
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.emailAddress = emailAddress;
+        this.memberType = memberType;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public int getAge()
+    {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
     
-//    public int getAge(LocalDate dateOfBirth)
-//    {
-//       LocalDate.now()
-//    }
-    
+    public LocalDate getNextPaymentDate()
+    {
+        int monthOfJoining = dateOfJoining.getMonthValue();
+        int dayOfJoining = dateOfJoining.getDayOfMonth();
+        int currentYear = LocalDate.now().getYear();
+        
+        return LocalDate.of(currentYear, monthOfJoining, dayOfJoining);
+    }
+
 }
