238dd0cfff8bf176e55430cd4b811e3955245c5d
Kasper Jeppesen <Kasper Jeppesen@10.50.138.132>
2018-11-13 10:30:11 +0100

    fields på members

---
+++ Delfinen/src/Data/Member.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Kasper Jeppesen
+ */
+public class Member
+{
+
+    private boolean paidCurrentYear, activityStatus;
+    private int zipCode, memberID;
+    private long phoneNumber, arrearsBalance;
+
+    private String name, address, city, emailAddress, memberType;
+    private LocalDate dateOfBirth, dateOfJoining;
+    
+//    public int getAge(LocalDate dateOfBirth)
+//    {
+//       LocalDate.now()
+//    }
+    
+}
