e736ed3dd938d8f3bee7be623962544dcfb72c38
Freiden <Freiden@Freiden-PC>
2018-11-13 14:02:45 +0100

    Færdiggjort Member klassen med det der står i klassediagrammet pt.. Har lavet tests for at tjekke at mine metoder dur, men den kan godt bruge en lidt mere grundig test senere hen

---
+++ Delfinen/test/Data/MemberTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.time.LocalDate;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author Martin L.B.
+ */
+public class MemberTest
+{
+
+    private Member johanne;
+
+    public MemberTest()
+    {
+        johanne = new Member(false, false, 2800, 010, 14548645, 50000, "Johanne Jaeger",
+                "Fiktiv Vej 23", "Kongens Lyngby", "Johanne.j@test.dk",
+                "Competitive", LocalDate.of(1993, 04, 12),
+                LocalDate.of(2009, 12, 10));
+    }
+
+    //Mangler flere tests
+    @Test
+    public void testGetAge()
+    {
+        assertEquals(25, johanne.getAge());
+    }
+
+    @Test
+    public void testGetNextPayment()
+    {
+        assertEquals(LocalDate.of(2018, 12, 10), johanne.getNextPaymentDate());
+    }
+
+    @Test
+    public void testPayArrears()
+    {
+        assertEquals(50000, johanne.getArrearsBalance());
+        johanne.payArrears();
+        assertEquals(0, johanne.getArrearsBalance());
+    }
+
+    @Test
+    public void testRegisterPayment()
+    {
+        assertFalse(johanne.hasPaidCurrentYear());
+        johanne.registerPayment();
+        assertTrue(johanne.hasPaidCurrentYear());
+    }
+
+    @Test
+    public void testSwitchActivityStatus()
+    {
+        assertFalse(johanne.isActiveMember());
+        johanne.switchActivityStatus();
+        assertTrue(johanne.isActiveMember());
+    }
+}
