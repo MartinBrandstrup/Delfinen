9652c744dd99342be4f35e6525b3f4d88743e779
Freiden <Freiden@Freiden-PC>
2018-11-14 16:38:25 +0100

    Vi behøver da slet ikke en class MemberList... ændret i Controlleren til at implementere dette.

--- Delfinen/src/Logic/Controller.java
+++ Delfinen/src/Logic/Controller.java
@@ -20,22 +20,32 @@ public class Controller
 {
 
     private DataAccessor data;
-    private MemberList memberlist;
+    //private MemberList memberlist;
+    private ArrayList<Member> memberlist;
     private int MIDcounter = 1;
 
-    
     //MemberID og dateOfJoining bliver automatisk genereret.
     //Hvis der sker en fejl kan man kalde .set på medlemmet og .setMID på
     //controlleren for at ændre counteren
-    public void registerNewMember(boolean paidCurrentYear, 
-            boolean activityStatus, int zipCode, long phoneNumber, 
-            long arrearsBalance, String name, String address, String city, 
+    public void registerNewMember(boolean paidCurrentYear,
+            boolean activityStatus, int zipCode, long phoneNumber,
+            long arrearsBalance, String name, String address, String city,
             String email, String memberType, LocalDate dateOfBirth)
     {
-        memberlist.getMemberlist().add(new Member(paidCurrentYear, activityStatus, zipCode, 
-                MIDcounter, phoneNumber, arrearsBalance, name, address, city, email, 
+        memberlist.add(new Member(paidCurrentYear, activityStatus, zipCode,
+                MIDcounter, phoneNumber, arrearsBalance, name, address, city, email,
                 memberType, dateOfBirth, LocalDate.now()));
         ++MIDcounter;
+    }
+
+    public ArrayList<Member> getMemberlist()
+    {
+        return memberlist;
+    }
+
+    public void getMemberListFromFile(ArrayList members)
+    {
+        this.memberlist = data.getMemberList();
     }
 
     public int getMIDcounter()
@@ -46,10 +56,5 @@ public class Controller
     public void setMIDcounter(int MIDcounter)
     {
         this.MIDcounter = MIDcounter;
-    }
-    
-    public ArrayList<Member> saveMemberListToFile()
-    {
-        return memberlist.getMemberlist();
     }
 }
