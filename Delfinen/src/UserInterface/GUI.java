463c2987acc9605aa8765f9d55dd6b820a3a4f83
Kasper Jeppesen <Kasper Jeppesen@10.50.138.132>
2018-11-22 11:56:38 +0100

    GUI

--- Delfinen/src/UserInterface/GUI.java
+++ Delfinen/src/UserInterface/GUI.java
@@ -24,7 +24,7 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author Kasper Jeppesen
  */
-public class Main extends javax.swing.JFrame
+public class GUI extends javax.swing.JFrame
 {
 
     private CardLayout card;
@@ -36,7 +36,7 @@ public class Main extends javax.swing.JFrame
     /**
      * Creates new form Main
      */
-    public Main()
+    public GUI()
     {
         initComponents();
         card = (CardLayout) mainPanel.getLayout();
@@ -1691,7 +1691,7 @@ public class Main extends javax.swing.JFrame
         }
         catch(Exception ex)
         {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         model = (DefaultTableModel) MembersTable.getModel(); //Hver lige opmærksom på om denne her ødelægger det
@@ -1761,7 +1761,7 @@ public class Main extends javax.swing.JFrame
         }
         catch (Exception ex)
         {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         lastTeamAddedTable(); //is choosing which table the field model appear as
@@ -2197,20 +2197,21 @@ public class Main extends javax.swing.JFrame
         }
         catch(ClassNotFoundException ex)
         {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         catch(InstantiationException ex)
         {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         catch(IllegalAccessException ex)
         {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         catch(javax.swing.UnsupportedLookAndFeelException ex)
         {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
@@ -2218,7 +2219,7 @@ public class Main extends javax.swing.JFrame
         {
             public void run()
             {
-                new Main().setVisible(true);
+                new GUI().setVisible(true);
             }
         });
     }
